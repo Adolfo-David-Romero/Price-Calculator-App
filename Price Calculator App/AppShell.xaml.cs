f379553c7bf4b45b530a7a89fe0747033d5fa4fd
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2025-02-03 14:45:44 -0500

    Initial Commit

---
+++ Price Calculator App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Price_Calculator_App;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Price Calculator App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Price_Calculator_App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}

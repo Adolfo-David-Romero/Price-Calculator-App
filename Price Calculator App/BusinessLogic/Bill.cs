b681b2ec230421e77063cf592532026a8c70dfd0
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2025-02-04 20:00:20 -0500

    Created BuisnessLogic dir and initially created Bill.cs class.

---
+++ Price Calculator App/BusinessLogic/Bill.cs
@@ -0,0 +1,11 @@
+namespace Price_Calculator_App.BusinessLogic;
+
+public class Bill
+{
+    private double pricePerItem;
+    private double discount;
+    private int nummberOfItems;
+    private const double taxRate = 0.13;
+    
+
+}
